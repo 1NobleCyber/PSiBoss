9a47f6a018114f75cae06f5f3c6f7cb8e8939604
1NobleCyber <206953920+1NobleCyber@users.noreply.github.com>
2026-02-01 22:06:33 -0600

    Initial Version

---
+++ src/Public/Get-iBossLogTables.ps1
@@ -0,0 +1,32 @@
+function Get-iBossLogTables {
+    <#
+    .SYNOPSIS
+        Retrieves the list of available log tables (archives) from the iBoss Reporting Service.
+
+    .DESCRIPTION
+        Sends a request to the iBoss Reporting Node to fetch all available log archives, 
+        including log reports if specified. 
+        Endpoint: /ibreports/web/log/ips/archives
+
+    .EXAMPLE
+        Get-iBossLogTables
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $false)]
+        [ValidateSet('url', 'ips')]
+        [string]$LogFamily = 'url'
+    )
+
+    process {
+        if (-not $Global:iBossSession) {
+            throw "Not connected. Please run Connect-iBoss first."
+        }
+
+        $Uri = "/ibreports/web/log/$LogFamily/archives?includeAllRecord=true&includeLogReports=true"
+        
+        $Result = Invoke-iBossRequest -Service Reporting -Uri $Uri -Verbose:$VerbosePreference
+
+        return $Result
+    }
+}
